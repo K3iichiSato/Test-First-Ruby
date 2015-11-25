66c5d297ce3889579c2cc5dcb3f9e4c9b2000f78
Keiichi Sato <nanoks@gmail.com>
2015-11-25 00:35:27 -0500

    done

--- 15_in_words/lib/in_words.rb
+++ 15_in_words/lib/in_words.rb
@@ -5,25 +5,52 @@ class Fixnum
 		teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
 		tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
 	
+		numdigits = number.to_s.length 
+		display = ''
 
-		if number< 10
-		ones[number]
-		elsif number>=10 && number<20
-		teens[number-10]
-		elsif number>=20 && number<100
-		tenz = number/10
-		tenw = tens[tenz-1]
-		left = number % 10
-			if left > 0
-			tenw + ' ' + ones[left]
-			else
-			tenw
+		auto = Proc.new {|number,zero,name|
+			display << number.to_s[0..-zero].to_i.in_words
+			display << " #{name}"
+			right = number.to_s[-(zero-1)..-1]
+			allzero = true
+			right.chars.each do |n|
+    			if n != '0'
+        		allzero = false
+        		break
+    			end
 			end
-		else
-		hundz = number/100
-		ones[hundz] + ' ' + 'hundred'
+				if !allzero
+					display << ' ' + right.to_i.in_words
+				end
+		}
+
+		if numdigits==1 
+		display << ones[number]
+		elsif numdigits==2
+			if number < 20
+				display << teens[number-10]
+			else
+				display << tens[(number/10) - 1]
+				if number.to_s[-1] != '0'
+					display << ' ' + ones[number.to_s[-1].to_i]
+				end 
+			end
+		elsif numdigits == 3
+			display << ones[number.to_s[0].to_i]
+			display << " hundred"
+				if number.to_s[-2..-1] != '00'
+					display << ' ' + number.to_s[-2..-1].to_i.in_words
+				end
+		elsif numdigits >= 4 && numdigits < 7
+			auto.call(number,4,'thousand')
+		elsif numdigits >= 7 && numdigits < 10
+			auto.call(number,7,'million')
+		elsif numdigits >= 10 && numdigits < 13
+			auto.call(number,10,'billion')
+		elsif numdigits >= 13 && numdigits < 16
+			auto.call(number,13,'trillion')
 		end
-		
+		display
 
 	end
 end
